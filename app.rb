c2923eeb20c7584250c6dc6a6966f7f08f2df3f8
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-15 05:15:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,9 +29,10 @@ class App < Sinatra::Base
   end
 
   get /:operation/:number1/:number2
-      @number1=params[:number1]
-      @number2=params[:number2]
-
+      @number1=params[:number1].to_i
+      @number2=params[:number2].to_i
+      @number = @number1 + @number2
+      @number.to_s
   end
 
 
