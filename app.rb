4a2fd7131a69ca12c79168681e42bbb38a8eada7
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-15 05:34:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,16 +32,16 @@ class App < Sinatra::Base
       @operations =params[:operation]
       @number1=params[:number1].to_i
       @number2=params[:number2].to_i
-      
+
       case operations
       when @add
-        @number1 + @number2
+        '#{@number1 + @number2}'
       when sub
-        @number2 - @number1
+        '#{@number2 - @number1}'
       when mult
-        @number2 * @number1
+        '#{@number2 * @number1}'
       when div
-        @number2 / @number1
+        '#{@number2 / @number1}'
       end
   end
 
