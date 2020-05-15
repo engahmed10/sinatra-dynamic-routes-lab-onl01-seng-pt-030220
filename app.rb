cc7ac497b6b399138d0cbbcd1b09aac5d2315966
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-15 04:56:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,8 +16,9 @@ class App < Sinatra::Base
   get 'say/:number/:phrase'  do
     @num=params[:number].to_i
     @phr=params[:phrase]
+    string=""
     @num.times do
-     "#{@phr}"
+     string +="#{@phr}"
     end
   end
 
