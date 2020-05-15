02e528b037429e93651e188c4877e414be9cb8de
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-15 05:35:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ class App < Sinatra::Base
       @number1=params[:number1].to_i
       @number2=params[:number2].to_i
 
-      case operations
+      case @operations
       when @add
         '#{@number1 + @number2}'
       when sub
